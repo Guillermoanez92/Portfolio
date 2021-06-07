40aff21aa6d6f7de0f6039cf94d6a0a91efacd3d
guillermoanez92 <guillermoanez92@gmail.com>
2021-06-07 18:30:18 -0500

    added fonts

--- lib/framework/prismic/templates/home.php
+++ lib/framework/prismic/templates/home.php
@@ -52,7 +52,7 @@ echo "</pre>";
 		<?php } ?>
 	</div>
 </section>
-<div class="lgos">
+<div class="logos">
 	<?php foreach ($data->skillset2_repeater2 as $key => $item) { ?>
 		<img src=<?php echo $item->icon->url ?>>
 	<?php } ?>
@@ -70,8 +70,8 @@ echo "</pre>";
 		<?php } ?>
 	</div>
 </section>
-<section class="social">
-	<div class="copy">
+<section class="social-module">
+	<div class="left">
 		<h2><?php echo $data->social_header[0]->text ?></h2>
 		<p><?php echo $data->social_copy[0]->text ?></p>
 		<a href="https://www.instagram.com/guillermo.anez/"><?php echo $data->social_anchor_tag[0]->text ?></a>
@@ -85,36 +85,42 @@ echo "</pre>";
 	<a href="https://dribbble.com/guillermoanez92"><?php echo $data->dribbble_anchor_tag[0]->text ?></a>
 	<img src=<?php echo $data->dribbble_image->url?>>
 </section>
+
 <section class="about">
-	<div class="copy">
+	<div class="left">
 		<h2><?php echo $data->reviews_header[0]->text ?></h2>
 		<p><?php echo $data->reviews_copy[0]->text ?></p>
 	</div>
 	<div class="reviews">
 		<?php foreach ($data->reviews_repeater as $key => $item) { ?>
-		<div class="single reviews">
-			<img src=<?php echo $item->icon->url ?>
-			 <h4><?php echo $item->copy[0]->text ?></h4>
-			<p class="employer"><?php echo $item->eyebrow1[0]->text ?></p>
-			<p><?php echo $item->eyebrow_2[0]->text ?></p>
-		</div>
+			<div class="single reviews">
+				<img src=<?php echo $item->icon->url ?>>
+				<h4><?php echo $item->copy[0]->text ?></h4>
+				<p class="employer"><?php echo $item->eyebrow1[0]->text ?></p>
+				<p><?php echo $item->eyebrow_2[0]->text ?></p>
+			</div>
 		<?php } ?>
 	</div>
 </section>
 <section class="hobbies">
 	<h2><?php echo $data->hobbies_header[0]->text ?></h2>
 	<p><?php echo $data->hobbies_copy[0]->text ?></p>
-	<div class="single hobbies">
+	<div class="tab-wrapper">
 		<?php foreach ($data->hobbies_repeater as $key => $item) { ?>
-		<div class="hobbie">
-			<h4><?php echo $item->title[0]->text ?></h4>
-			<img src=<?php echo $item->icon->url ?>>
-		</div>
+			<button><?php echo $item->title[0]->text ?></button>
+		<?php } ?>
+	</div>
+
+	<div class="all-hobby-images">
+		<?php foreach ($data->hobbies_repeater as $key => $item) { ?>
+			<div class="hobby-images">
+				<img src=<?php echo $item->icon->url ?>>
+			</div>
 		<?php } ?>
 	</div>
 </section>
 <section id="contact" class="contact">
-	<div class="copy">
+	<div class="left">
 		<h2><?php echo $data->contact_header[0]->text ?></h2>
 		<p><?php echo $data->contact_copy[0]->text ?></p>
 	</div>
